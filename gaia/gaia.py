0f173e67f8683da6e32222731004488b808d9559
deadlylaid <deadlylaid@gmail.com>
2018-08-30 00:12:34 +0900

    file create when command find

--- gaia/gaia.py
+++ gaia/gaia.py
@@ -1,10 +1,28 @@
 import click
+import json
+import os
 
-@click.command()
+
+@click.group()
 def cli():
-    """
-    Example Code
+    os.system('rm -rf logs')
+    pass
 
-    :return:
-    """
-    click.echo('Hello World!')
+
+@cli.command()
+@click.argument('bucket_name')
+def find(bucket_name):
+    try:
+        with open('gaia_conf.json') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError("gaia_conf.json file doesn't exist, create gaia_conf.json in root directory")
+
+    if not os.path.isdir('logs'):
+        os.mkdir('logs')
+    os.mkdir('logs/'+bucket_name)
+
+    try:
+        bucket_path = config['BUCKET_PATH'][bucket_name]
+    except KeyError:
+        raise KeyError(bucket_name + ' is not in gaia_conf.json')
