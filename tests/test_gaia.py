ad832e799f06f5ddf911e18a05a12932e12064a3
deadlylaid <deadlylaid@gmail.com>
2018-09-14 00:00:50 +0900

    append bucketname, path in gen command

--- tests/test_gaia.py
+++ tests/test_gaia.py
@@ -1,4 +1,5 @@
 import ast
+import json
 import pytest
 import mock
 import os
@@ -44,3 +45,14 @@ def test_log_finder(keyword):
     result = gaia._log_finder('tests/test_logs/', keyword)
     result = ast.literal_eval(result)
     assert result['uid'] == keyword
+
+
+# test_gen은 test_find보다 늦게 실행되므로 gaia_conf.json을 삭제해도 영향을 미치지 않는다.
+def test_gen():
+    os.system('rm gaia_conf.json')
+    runner = CliRunner()
+    runner.invoke(gaia.cli, ['gen', 'testbucket', 'testpath'])
+    assert os.path.isfile('gaia_conf.json') == True
+    with open('gaia_conf.json') as f:
+        data = json.load(f)
+    assert data == {'BUCKET_PATH': {'testbucket': 'testpath'}}
